b3b5f329f22cdcbee2ff0fa969bd56594c8a1cd4
ninja <ninja@DESK-TALES>
2023-04-15 21:23:48 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Exercicio1.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Cálculo da área do círculo. Entre com o raio:");
+double pi = 3.14159, R, A;
+R = Double.Parse(Console.ReadLine());
+A = pi * R * R;
+Console.WriteLine("A = " + A.ToString("F4"));
+// referência: https://learn.microsoft.com/pt-br/dotnet/api/system.double.tostring?view=net-7.0
