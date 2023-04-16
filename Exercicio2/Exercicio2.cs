775407f68deb2e6e18a5b75af7370aac3b30a754
ninjabsbx <ninjaabsbx@gmail.com>
2023-04-16 12:50:20 -0300

    Exercício 2 pronto

---
+++ Exercicio2/Exercicio2.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("calcule a SOMA! Digite qualquer valor a seguir:");
+int A, B, SOMA;
+A = int.Parse(Console.ReadLine());
+B = int.Parse(Console.ReadLine());
+SOMA = A + B;
+Console.WriteLine("SOM = " + SOMA);
