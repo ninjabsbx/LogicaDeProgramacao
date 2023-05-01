389acf0aa66124d6fef06244287f125b6ac2e3f3
ninjabsbx <ninjaabsbx@gmail.com>
2023-05-01 16:06:34 -0300

    exercicio6

--- Exercicio 4/Exercicio4.cs
+++ Exercicio 4/Exercicio4.cs
@@ -4,10 +4,10 @@ decimal A, B, MEDIA;
 Console.WriteLine("Entre com a primeira nota. Deve ser um numero entre 0,0 e 10,0, com uma casa decimal.");
 A = Decimal.Parse(Console.ReadLine());
 if (A < 0 || A > 10 || decimal.Round(A, 1) != A)
-{
+   {
     Console.WriteLine("Erro do formato. Deve ser um numero entre 0,0 e 10,0, com uma casa decimal.");
 } else
-{
+    {
     Console.WriteLine("Entre com a segunda nota. Deve ser um numero entre 0,0 e 10,0, com uma casa decimal.");
     B = Decimal.Parse(Console.ReadLine());
     if (B < 0 || B > 10 || decimal.Round(B, 1) != B)
---
+++ Exercicio6/Exercicio6.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+string line;
+StreamReader sr = new StreamReader("compras.txt");
+line = sr.ReadLine();
+decimal total = 0;
+while (line != null)
+{
+    string[] fields = line.Split(' ');
+    int codigo = int.Parse(fields[0]), quantidade = int.Parse(fields[1]);
+    decimal preco = decimal.Parse(fields[2]);
+    total += quantidade * preco;
+    //System.Console.WriteLine($"PRODUTO - codigo: {codigo} - quantidade: {quantidade} - preco: {preco} - total: {total}");
+    line = sr.ReadLine();
+}
+sr.Close();
+System.Console.WriteLine($"VALOR A PAGAR: R$ {total.ToString("F2")}");
