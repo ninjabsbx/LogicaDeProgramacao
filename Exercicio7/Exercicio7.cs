7c308eaccc02391e3786da95773687c4d66a81f3
ninjabsbx <ninjaabsbx@gmail.com>
2023-05-01 16:41:15 -0300

    exercicio 8

---
+++ Exercicio7/Exercicio7.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Calcule a area da circuferencia");
+double pi = 3.14159, R, A;
+R = Double.Parse(Console.ReadLine());
+A = pi * R * R;
+Console.WriteLine("A = " + A.ToString("F4"));
